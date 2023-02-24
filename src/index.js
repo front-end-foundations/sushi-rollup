cb19a932521a4b5b6f53a0711a0939434a10d42c
DannyBoyNYC <daniel.deverell@gmail.com>
2023-02-23 19:42:14 -0500

    done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { random } from "./modules/utils.js";
+import { showPopover } from "./modules/modal.js";
+import { basilChefs, basilTexture } from "./modules/chefs.js";
+import { getQuote } from "./modules/api.js";
+
+const API = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+const el = document.querySelector("h2");
+const recipeName = `${random(basilChefs)}'s ${random(basilTexture)} pesto`;
+
+getQuote(API, ".quote");
+el.innerHTML = recipeName;
+
+console.log("ELEMENT: ", el);
+
+document.addEventListener("click", showPopover);
